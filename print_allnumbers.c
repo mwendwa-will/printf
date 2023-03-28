416c1a377bf6efbf93d85c45af4a3f4a39858ec1
AnthonyMuneneAM <anthonymunene986@gmail.com>
2023-03-28 22:56:47 +0300

    Test files

--- print_allnumbers.c
+++ print_allnumbers.c
@@ -2,24 +2,47 @@
 
 /**
  * print_int - prints an integer
- * @l: va_list of arguments from _printf
- * @f: pointer to the struct flags determining
- * if a flag is passed to _printf
+ * @l: list of arguments
+ * @f: pointer to the flags determining if a flag is passed
+ *
  * Return: number of char printed
  */
 int print_int(va_list l, flags_t *f)
 {
 	int n = va_arg(l, int);
-	int res = count_digit(n);
-
+	int result = count_digit(n);
+	
 	if (f->space == 1 && f->plus == 0 && n >= 0)
-		res += _putchar(' ');
+		result += _putchar(' ');
 	if (f->plus == 1 && n >= 0)
-		res += _putchar('+');
+		result += _putchar('+');
 	if (n <= 0)
-		res++;
+		result++;
 	print_number(n);
-	return (res);
+	return (result);
+}
+
+/**
+ * count_digit - returns the number of digits in an integer for _printf
+ * @i: integer to evaluate
+ *
+ * Return: number of digits
+ */
+int count_digit(int i)
+{
+	unsigned int d = 0;
+	unsigned int u;
+
+	if (i < 0)
+		u = i * -1;
+	else
+		u = i;
+	while (u != 0)
+	{
+		u /= 10;
+		d++;
+	}
+	return (d);
 }
 
 /**
@@ -29,7 +52,7 @@ int print_int(va_list l, flags_t *f)
  * if a flag is passed to _printf
  * Return: number of char printed
  */
-int print_unsigned(va_list l, flags_t *f)
+ int print_unsigned(va_list l, flags_t *f)
 {
 	unsigned int u = va_arg(l, unsigned int);
 	char *str = convert(u, 10, 0);
@@ -37,6 +60,7 @@ int print_unsigned(va_list l, flags_t *f)
 	(void)f;
 	return (_puts(str));
 }
+
 
 /**
  * print_number - helper function that loops through
@@ -58,27 +82,4 @@ void print_number(int n)
 	if (n1 / 10)
 		print_number(n1 / 10);
 	_putchar((n1 % 10) + '0');
-}
-
-/**
- * count_digit - returns the number of digits in an integer
- * for _printf
- * @i: integer to evaluate
- * Return: number of digits
- */
-int count_digit(int i)
-{
-	unsigned int d = 0;
-	unsigned int u;
-
-	if (i < 0)
-		u = i * -1;
-	else
-		u = i;
-	while (u != 0)
-	{
-		u /= 10;
-		d++;
-	}
-	return (d);
 }
---
+++ test/README.md
@@ -0,0 +1,6 @@
+# Tests
+Compile with:
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 ../*.c main.c -o a
+
+Run executable with:
+./a
